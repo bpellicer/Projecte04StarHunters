cca8fe19d0db4823bf13b7772bcdc24d6cf6fa18
bernat <b.pellicer@sapalomera.cat>
2021-03-02 17:06:20 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+	<meta charset="UTF-8">
+</head>
+<body>
+    <div>
+        <button type="button"></button>
+    </div>
+    <div>
+        <button type="button"></button>
+    </div>
+</body>
+</html>
