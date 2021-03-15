a1bdb95cba5083eb6f6ef58e5418ff61f575dfda
Jordi Hernandez <jjordihm9@gmail.com>
2021-03-15 20:00:33 +0100

    move spaceship with keyboard

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ const HEIGHT = GAME_ZONE.height(); // height of the game zone
 let stars = [];
 let endGame = false;
 
+let moveXAxis; // interval to move in 'x' axis
+let moveYAxis; // interval to move in 'y' axis
+
 let spaceship = new Spaceship('player-spaceship');
 
 let generateStars = setInterval(() => {
@@ -16,86 +19,52 @@ let generateStars = setInterval(() => {
 	stars.push(new Star()); // add the star to the list
 }, 1000 * 1.5);
 
+/**
+ * Listen for 'keydown' and 'keyup' events on document to move the spaceship
+ */
+function keyEvents() {
+	$(document).on('keydown keyup', e => {
+		const LEFT_KEY = e.key === 'a' || e.code === 'KeyA' || e.keyCode === 65;
+		const RIGHT_KEY = e.key === 'd' || e.code === 'KeyD' || e.keyCode === 68;
+		const UP_KEY = e.key === 'w' || e.code === 'KeyW' || e.keyCode === 87;
+		const DOWN_KEY = e.key === 's' || e.code === 'KeyS' || e.keyCode === 83;
 
-function keyDownEvents(){
-    document.addEventListener('keydown',(event) => {
-        const keyName = event.key;
-        switch(keyName.toLowerCase()){
-            case "arrowleft": case "a":
-                spaceship.moveLeft=true;
-            break;
-            case "arrowright": case "d":
-                spaceship.moveRight = true;
-            break;
-            case "arrowdown": case "s":
-                spaceship.moveDown = true;
-            break;
-            case "arrowup": case "w":
-                spaceship.moveUp = true;
-            break;
-        }
-    },false);
+		if (e.type === 'keydown') {
+			if (LEFT_KEY || RIGHT_KEY) {
+				//if is moving, leave
+				if (moveXAxis) return;
+				// check in which direction to move
+				let x = LEFT_KEY ? -1 : RIGHT_KEY ? 1 : 0;
+				// start moving
+				moveXAxis = setInterval(() => {
+					spaceship.move(x, 0);
+				}, 5);
+			} else if (UP_KEY || DOWN_KEY) {
+				// if is moving, leave
+				if (moveYAxis) return;
+				// check in which direction move
+				let y = UP_KEY ? -1 : DOWN_KEY ? 1 : 0;
+				// start moving
+				moveYAxis = setInterval(() => {
+					spaceship.move(0, y);
+				}, 5);
+			}
+		} else if (e.type === 'keyup') {
+			if (LEFT_KEY || RIGHT_KEY) {
+				clearInterval(moveXAxis);
+				moveXAxis = null;
+			} else if (UP_KEY || DOWN_KEY) {
+				clearInterval(moveYAxis);
+				moveYAxis = null;
+			}
+		}
+	});
 }
 
-function keyUpEvents(){
-    document.addEventListener('keyup',(event) => {
-        const keyName = event.key;
-        switch(keyName.toLowerCase()){
-            case "arrowleft": case "a":
-                spaceship.moveLeft=false;
-            break;
-            case "arrowright": case "d":
-                spaceship.moveRight = false;
-            break;
-            case "arrowdown": case "s":
-                spaceship.moveDown = false;
-            break;
-            case "arrowup": case "w":
-                spaceship.moveUp= false;
-            break;
-        }
-    },false);   
+function init() {
+	keyEvents();
 }
 
-function checkKeys(){
-    if((spaceship.moveLeft === true && spaceship.moveRight === true) || (spaceship.moveUp === true && spaceship.moveDown === true)){
-		spaceship.move(spaceship.xPos,spaceship.yPos);
-	}
-	else if(spaceship.moveLeft === true  && spaceship.moveUp == false){
-		if(spaceship.xPos>=22)spaceship.move(spaceship.xPos-1,spaceship.yPos);
-	}
-    /*
-	else if(destructor.rightKey === true && destructor.upKey == false){
-		if(destructor.xPos<=WIDTH-22)destructor.move(destructor.xPos+1,destructor.yPos);
-    }
-    else if(destructor.downKey === true && destructor.leftKey == false){
-		if(destructor.yPos<=HEIGHT)destructor.move(destructor.xPos,destructor.yPos+1);
-    }
-    else if(destructor.upKey === true && destructor.leftKey == false){
-		if(destructor.yPos<=HEIGHT)destructor.move(destructor.xPos,destructor.yPos-1);
-    }
-    else if(destructor.downKey === true && destructor.rightKey == false){
-		if(destructor.yPos<=HEIGHT)destructor.move(destructor.xPos,destructor.yPos+1);
-    }
-    else if(destructor.upKey === true && destructor.rightKey == false){
-		if(destructor.yPos<=HEIGHT)destructor.move(destructor.xPos,destructor.yPos-1);
-    }
-    else if(destructor.downKey === true && destructor.leftKey === true){
-		if(destructor.yPos<=HEIGHT && destructor.xPos>=22)destructor.move(destructor.xPos-1,destructor.yPos+1);
-    }
-    else if(destructor.downKey === true && destructor.rightKey === true){
-		if(destructor.yPos<=HEIGHT && destructor.xPos<=WIDTH-22)destructor.move(destructor.xPos+1,destructor.yPos+1);
-    }
-    else if(destructor.upKey === true && destructor.rightKey === true){
-		if(destructor.yPos<=HEIGHT && destructor.xPos<=WIDTH-22)destructor.move(destructor.xPos+1,destructor.yPos-1);
-    }
-    else if(destructor.upKey === true && destructor.leftKey === true){
-		if(destructor.yPos<=HEIGHT && destructor.xPos>=22)destructor.move(destructor.xPos-1,destructor.yPos+1);
-    }*/
-}
-
-function _init(){
-    keyDownEvents();
-    keyUpEvents();
-    setInterval(checkKeys,1);
-}
+$(document).ready(function () {
+	init();
+});
