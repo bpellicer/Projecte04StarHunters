3b6121b732b0679c603fa61c602c8cfc38afb947
Jordi Hernandez <jjordihm9@gmail.com>
2021-03-16 17:44:21 +0100

    added 4 cornes coordinates as object property of spaceship & star classes

--- js/Spaceship.js
+++ js/Spaceship.js
@@ -1,17 +1,31 @@
 'use strict';
 
 class Spaceship {
-	constructor(id) {
-		this.id = id; // name of the player
-		this.spaceship = $(`#${id}`);
+	constructor(nickname) {
+		this.nickname = nickname; // name of the player
+		this.spaceship = $(`#${nickname}`);
 
 		this.xPos = 0;
 		this.yPos = 0;
 
-		this.moveLeft = false;
-		this.moveRight = false;
-		this.moveUp = false;
-		this.moveDown = false;
+		this.coordinates = {
+			'topLeft': {
+				'x': 0,
+				'y': 0,
+			},
+			'bottomLeft': {
+				'x': 0,
+				'y': 0,
+			},
+			'topRight': {
+				'x': 0,
+				'y': 0,
+			},
+			'bottomRight': {
+				'x': 0,
+				'y': 0,
+			},
+		}
 
 		// get the css value of width and height, remove the 'px'
 		this.width = Number(this.spaceship.css('width').replace('px',''));
@@ -46,5 +60,25 @@ class Spaceship {
 			'left': `${this.xPos}px`,
 			'top': `${this.yPos}px`
 		});
+
+		this.calcCoordinates();
+	}
+
+	/**
+	 * Recalculate the coordinates of the 4 vertex of the star
+	 */
+	calcCoordinates() {
+		// Top Left coordinates
+		this.coordinates.topLeft.y = this.yPos;
+		this.coordinates.topLeft.x = this.xPos;
+		// Bottom Left coordinates
+		this.coordinates.bottomLeft.x = this.xPos;
+		this.coordinates.bottomLeft.y = this.yPos + this.height;
+		// Top Right coordinates
+		this.coordinates.topRight.x = this.xPos + this.width;
+		this.coordinates.topRight.y = this.yPos;
+		// Bottom Right coordinates
+		this.coordinates.bottomRight.x = this.xPos + this.width;
+		this.coordinates.bottomRight.y = this.yPos + this.height;
 	}
 }
--- js/Star.js
+++ js/Star.js
@@ -15,6 +15,28 @@ class Star {
 		this.xPos = randNum(0, WIDTH - 64);
 		this.yPos = randNum(0, HEIGHT - 64);
 
+		this.coordinates = {
+			'topLeft': {
+				'x': 0,
+				'y': 0,
+			},
+			'bottomLeft': {
+				'x': 0,
+				'y': 0,
+			},
+			'topRight': {
+				'x': 0,
+				'y': 0,
+			},
+			'bottomRight': {
+				'x': 0,
+				'y': 0,
+			},
+		}
+
+		this.width = 64;
+		this.height = 64;
+
 		// create the star image element, set it up and append to game zone
 		this.star = $('<img>')
 			.attr('alt', '8 bits star')
@@ -25,6 +47,26 @@ class Star {
 				'top': `${this.yPos}px`
 			})
 			.appendTo(GAME_ZONE);
+		
+		this.calcCoordinates();
+	}
+
+	/**
+	 * Recalculate the coordinates of the 4 vertex of the spaceship
+	 */
+	calcCoordinates() {
+		// Top Left coordinates
+		this.coordinates.topLeft.y = this.yPos;
+		this.coordinates.topLeft.x = this.xPos;
+		// Bottom Left coordinates
+		this.coordinates.bottomLeft.x = this.xPos;
+		this.coordinates.bottomLeft.y = this.yPos + this.height;
+		// Top Right coordinates
+		this.coordinates.topRight.x = this.xPos + this.width;
+		this.coordinates.topRight.y = this.yPos;
+		// Bottom Right coordinates
+		this.coordinates.bottomRight.x = this.xPos + this.width;
+		this.coordinates.bottomRight.y = this.yPos + this.height;
 	}
 
 }
